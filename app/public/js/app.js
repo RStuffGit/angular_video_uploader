8188bebd13d0afe238cd23f69d6bc19aa6f20bad
roman <rommi95@mail.ru>
2018-02-26 15:54:06 +0300

    add working uploader last

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -4,31 +4,5 @@
     angular.module('app', [
         'blueimp.fileupload'
     ])
-        .controller('DemoFileUploadController', [
-            '$scope', '$http', '$timeout', 'fileUploadService', '$filter', '$window',
-            function ($scope, $http, $timeout, fileUploadService) {
-                $scope.progress = 0;
-                $scope.videos = [];
-                $scope.uploadVideo = function (file) {
-                    fileUploadService.uploadFileToUrl(file).then(function (data) {
-                        $scope.videos.push(data.data.hashed_id);
-                        $scope.clearQueue();
-                    }, function (res) {
-                        alert(res.data.error);
-                        $scope.clearQueue();
-                    }, function (e) {
-                        $scope.progress = Math.floor((e.loaded / e.total) * 100);
-                    })
-                };
-
-                $scope.clearQueue = function () {
-                    $scope.queue = [];
-                    $timeout(function () {
-                        $scope.progress = 0;
-                    }, 1000);
-                }
-
-            }
-        ])
 
 }());
---
+++ app/public/js/components/wistiaUploader/wistiaUploader.component.js
@@ -0,0 +1,32 @@
+angular.module('app').component('wistiaUploader', {
+    templateUrl: '/js/components/wistiaUploader/wistiaUploader.template.html',
+    controller: [
+        '$scope', '$http', '$timeout', 'fileUploadService', '$filter', '$window',
+        function ($scope, $http, $timeout, fileUploadService) {
+            var that = this;
+            this.progress = 0;
+            this.videos = [];
+            this.uploadVideo = function (file, clearCallback) {
+                fileUploadService.uploadFileToUrl(file).then(function (data) {
+                    that.videos.push(data.data.hashed_id);
+                    that.clearQueue(file, clearCallback);
+                }, function (res) {
+                    alert(res.data.error);
+                    that.clearQueue(file, clearCallback);
+                }, function (e) {
+                    that.progress = Math.floor((e.loaded / e.total) * 100);
+                })
+            };
+
+            this.clearQueue = function (file, clearCallback) {
+                if (typeof clearCallback === "function") {
+                    clearCallback([file]);
+                }
+                $timeout(function () {
+                    that.progress = 0;
+                }, 1000);
+            }
+
+        }
+    ]
+});
